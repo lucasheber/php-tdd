5c400e44ecf229c3eb95c560b3ce81ad3fb96e20
Lucas Heber <lucas.heber07@gmail.com>
2020-08-04 14:14:05 -0300

    [add] test methods

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -22,4 +22,30 @@ class MaiorEMenorTest extends PHPUnit
         $this->assertEquals("Jogo de Pratos", $maiorMenor->getMenor()->getNome());
         $this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
     }
+
+    public function testOrdemCrescente()
+    {
+        $carrinho = new CarrinhoDeCompras();
+        $carrinho->adiciona(new Produto("Jogo de Pratos", 70));
+        $carrinho->adiciona(new Produto("Liquidificaro", 250));
+        $carrinho->adiciona(new Produto("Geladeira", 450));
+
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals("Jogo de Pratos", $maiorMenor->getMenor()->getNome());
+        $this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
+    }
+
+    public function testApenasUmProduto()
+    {
+        $carrinho = new CarrinhoDeCompras();
+        $carrinho->adiciona(new Produto("Jogo de Pratos", 70));
+
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals("Jogo de Pratos", $maiorMenor->getMenor()->getNome());
+        $this->assertEquals("Jogo de Pratos", $maiorMenor->getMaior()->getNome());
+    }
 }
